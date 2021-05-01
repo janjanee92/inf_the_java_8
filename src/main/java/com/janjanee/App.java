036b2976afb20faa3450f8df3a241909a5162e45
rowanlee92 <watrv41@gmail.com>
2021-05-01 10:52:51 +0900

    Add: Stream

--- src/main/java/com/janjanee/App.java
+++ src/main/java/com/janjanee/App.java
@@ -1,11 +1,32 @@
 package com.janjanee;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Spliterator;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class App {
 
     public static void main(String[] args) {
-        Foo foo = new DefaultFoo("Hello");
-        foo.printValue();
-        foo.printValueLowerCase();
-        Foo.printHello();
+        List<String> names = new ArrayList<>();
+
+        names.add("janjanee");
+        names.add("jihan");
+        names.add("bare");
+        names.add("foo");
+
+        List<String> collect = names.stream()
+                .map(s -> {
+                    System.out.println(s);
+                    return s.toUpperCase();
+                }).collect(Collectors.toList());
+
+        collect.forEach(System.out::println);
+
+        System.out.println("==============");
+
+        names.forEach(System.out::println);
     }
 }
